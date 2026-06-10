f6d60a23d7be9b2da8f54dd78b7c966629146efa
Chiranjeev Kumar <chiranjeevk171@gmail.com>
2026-06-10 09:24:27 +0530

    Fix: Removed duplicate taskRoutes declaration

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoutes');
-const taskRoutes = require('./src/routes/taskRoutes');
 const goalRoutes = require('./routes/goalRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
